672b6f1385445b17a15ce3c1c9a90f50dc03b600
Peter Witt <peter.witt.joergensen@gmail.com>
2018-11-18 11:09:09 +0100

    Updated sounds

--- Assets/bronyTrigger.cs
+++ Assets/bronyTrigger.cs
@@ -6,6 +6,9 @@ public class bronyTrigger : MonoBehaviour
 {
     public BronyController controller;
     public Spawner spawner;
+    public AudioSource audioSource;
+    public AudioClip introClip;
+    public AudioClip BossClip;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -13,6 +16,20 @@ public class bronyTrigger : MonoBehaviour
         {
             controller.Activate();
             spawner.gameObject.SetActive(false);
+            audioSource.Stop();
+            audioSource.clip = introClip;
+            audioSource.Play();
+            audioSource.volume = .5f;
+            StartCoroutine(playNext(audioSource.clip.length));
         }
+    }
+
+    IEnumerator playNext(float time)
+    {
+        yield return new WaitForSeconds(time);
+        audioSource.Stop();
+        audioSource.clip = BossClip;
+        audioSource.Play();
+        audioSource.volume = .5f;
     }
 }
